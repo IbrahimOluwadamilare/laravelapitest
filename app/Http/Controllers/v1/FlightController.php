89bb3ed995aad09a64a4125dbaf9dacbf7a5483a
Ibrahim Oluwadamilare <Oluwadamilare@Oluwadamilare.local>
2017-07-26 10:28:03 +0100

    CRUD operation implemented already in the API

---
+++ app/Http/Controllers/v1/FlightController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers\v1;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Services\v1\FlightService;
+
+class FlightController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    protected $flights;
+    public function __construct(FlightService $service){
+        $this->flights =$service;
+    }
+
+
+    public function index()
+    {
+        //call service
+        //return data
+        $parameters = request()->input();
+        $data = $this->flights->getFlights($parameters);
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        //$arrivalDateTime = $request ->input('arrival.datetime');
+
+        //return $arrivalDateTime;
+        try{
+
+        $flight =  $this->flights->createFlight($request);
+        return response()->json($flight, 201);
+        }catch (Exception $e){
+            return response()->json(['message'=> $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $parameters = request()->input();
+        $parameters['flightNumber'] = $id;
+        $data = $this->flights->getFlight($parameters);
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::resource('/v1/flights', v1\FlightController::class, [
+    'except' => ['create','edit']
+]);
+
+// api/v1/flight
